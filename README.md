60288982acfed0d9ca784ff7e1a5dd20db6274a7
Reetika R <r3rastogi@gmail.com>
2017-03-30 18:44:46 -0700

    Basic Heroku deploy works

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-Hola, Mundo!
+## Hola, Mundo!
 
 A translation app that will take in a phrase and return how to say it in various other languages
+
+Deployed at: https://bonjourlemonde.herokuapp.com/
--- index.js
+++ index.js
@@ -1,23 +1,17 @@
 var cool = require('cool-ascii-faces');
-var express = require('express');
+var express = require('express')
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index')
+app.get('/', function (req, res) {
+  res.send('Hello World!')
 });
 
 app.get('/cool', function(request, response) {
   response.send(cool());
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.listen(5000, function () {
+  console.log('Example app listening on port 5000!')
 });
